875309320573c8686fa83367f398caecf3bde9c2
cm2316 <cm2316@gmail.com>
2015-11-19 19:33:27 +0800

    init

--- app/models/User.scala
+++ app/models/User.scala
@@ -5,7 +5,13 @@ package models
   */
 
 object User{
+  val users = List(
+    User("bob@gmail.com","pass",true,10),
+    User("alic@gmail.com","pass",true,0),
+    User("john@gmail.com","pass",false,10)
+  )
 
+  def find(username:String):Option[User] = users.filter(_.username == username).headOption
 }
 
 case class User(username:String, password:String, isPremium:Boolean, balance:Int) {
